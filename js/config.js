cba79a99c0064bbb0976f544cf037cdc3de2f3a2
Nyquist <nyquist64@yahoo.com>
2024-09-20 20:26:33 +0200

    new domain config added

--- js/config.js
+++ js/config.js
@@ -2,9 +2,13 @@ if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
 	var appConfig = {
 		'base' : 'http://' + location.hostname + ":" + location.port,
 	}	
-} else {
+} else if(location.hostname === "cvetkovic-70.github.io") {
 	var appConfig = {
 		'base': 'https://cvetkovic-70.github.io/teodora-cvetkovic',
+	}
+} else {
+	var appConfig = {
+		'base': 'https://teodora-counseling.com',
 	}
 }
 
